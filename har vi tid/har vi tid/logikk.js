9e79975453e66dff7e1989d3dcf035044a93d952
sabl80 <sanne.blom.bijnen@gmail.com>
2026-01-16 11:06:37 +0100

    andre forsøk

---
+++ har vi tid/har vi tid/logikk.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const startKnapp = document.getElementById("start-knapp");
+
+  startKnapp.addEventListener("click", () => {
+    alert("Spillet starter snart!");
+  });
+});
